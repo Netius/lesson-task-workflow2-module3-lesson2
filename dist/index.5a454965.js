df1d3aab0ba542c6396af1abe0e0800a564b12ee
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2022-04-23 12:16:23 +0200

    Builded with parcel

--- dist/index.5a454965.js
+++ dist/index.5a454965.js
@@ -1,8 +1,8 @@
-
-async function postRequestAxios(){
+async function postRequestAxios() {
     const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
     console.log(response.data);
 }
 postRequestAxios();
-
 console.log("AXIOS");
+
+//# sourceMappingURL=index.5a454965.js.map
